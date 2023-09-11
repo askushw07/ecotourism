80803e1dcb1e639341342abbf93173d397c35785
Alok Singh <askushw07@gmail.com>
2023-09-11 15:49:28 +0530

    folder structure created

---
+++ ecotourism/src/Routes/AllRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route, Routes} from 'react-router'
+
+const AllRoutes = () => {
+  return (
+    <Routes>
+          
+    </Routes>
+  )
+}
+
+export default AllRoutes
